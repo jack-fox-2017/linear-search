cf0af5432656fa093b16c0ceae86be5853ba168f
Ahmad Nasikin <ahmad.nasikin09@gmail.com>
2017-07-25 19:06:05 +0700

    Linear Search Selesai

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,13 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+  console.log(values);
+  for(var i = 0; i < values.length; i++) {
+    if (target == values[i]) {
+      return i;
+    }
+  }
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,14 +22,22 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+  console.log(values);
+  var tamp = [];
+  for (var i = 0; i < values.length; i++) {
+    if (target == values[i]) {
+      tamp.push([i])
+    }
+  }
+  return tamp;
 }
 
 let banana_arr = "banana".split("");
 // ["b", "a", "n", "a", "n", "a"]
 console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
-
+console.log(globalLinearSearch("n", banana_arr));
+console.log(globalLinearSearch("b", banana_arr));
 module.exports = {
   linearSearch,
   globalLinearSearch
