10a63dcb3e714a8ed34e98f69beed1756dc4de17
Oscar Hermawan <email": null>
2017-07-25 15:29:56 +0700

    tama

--- linear_search.js
+++ linear_search.js
@@ -4,9 +4,17 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for (var i = 0; i < values.length; i++) {
+    if (target == values[i]) {
+      return +i
+    }
+    else {
+      return -1;
+    }
+  }
 }
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+let random_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37];
 
 console.log(linearSearch(18, random_numbers));
 // 2
@@ -17,6 +25,13 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let temp = [];
+  for (var i = 0; i < values.length; i++) {
+    if (target == values[i]) {
+      temp.push(i);
+    }
+  }
+  return temp;
 }
 
 let banana_arr = "banana".split("");
